32e211586c7c5a04941a896c3822574899fcd7ff
Wanderson Sena <wanderson.sena@abreu.pt>
2024-10-28 14:41:48 +0000

    Add bootstrap

--- Olympics.WebApp/src/app/app.component.ts
+++ Olympics.WebApp/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbNavModule  } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbNavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Olympics.WebApp';
+  active = 1;
 }
